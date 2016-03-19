39821aaebbeced355d7cd088e7d46e955c523c2f
notmyf4ulty <notmyf4ulty@github.com>
2016-03-19 18:46:21 +0100

    Some commented trash cleaned.[2]

--- main.cpp
+++ main.cpp
@@ -22,24 +22,6 @@ int main(int argc, char *argv[])
     dbConnector db;
     db.runDatabase();
 
-//    QSqlQueryModel *model = new QSqlQueryModel;
-//    model->setQuery("SELECT first_name, last_name FROM actor;");
-//    model->setHeaderData(0, Qt::Horizontal, "Name");
-//    model->setHeaderData(1, Qt::Horizontal, "Salary");
-
-//    QTableView *view = new QTableView;
-//    view->setModel(model);
-//    view->show();
-
-//    string cmd;
-
-//    do {
-//        qDebug() << "Type a query: ";
-//        getline(cin, cmd);
-//        db.runQueryInConsole(QString::fromStdString(cmd));
-//    }
-//    while (cmd != "exit");
-
     MainWindow mainWindow;
     mainWindow.show();
     return app.exec();
